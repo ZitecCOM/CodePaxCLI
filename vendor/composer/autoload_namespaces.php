572e1a5c12d233f62c2b9a5d6aa2bb2a636d70e1
Iulian Cristian GRIGORITA <iulian.grigorita@zitec.com>
2015-07-22 17:36:42 +0300

    Changed app structure in order to be compatible with PSR-0 and PSR-4

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CodePax_' => array($vendorDir . '/zitec', $baseDir . '/src/AppBundle/Model'),
+    '' => array($baseDir . '/src/AppBundle/Command'),
 );
